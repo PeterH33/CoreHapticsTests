0bb0ff8ddc5ae7165ecfdb25d71b27064c0126d2
Peter Hartnett <peter.hartnett@gmail.com>
2022-06-30 10:16:43 -0400

    Initial Commit

---
+++ CoreHapticsTests/CoreHapticsTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreHapticsTestsApp.swift
+//  CoreHapticsTests
+//
+//  Created by Peter Hartnett on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreHapticsTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
